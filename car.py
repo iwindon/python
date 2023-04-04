d2dcfa0850018882fe9f767ffd9561be41a93be7
Ivan Windon <ivan.windon@icloud.com>
2023-04-04 16:00:06 -0400

    working wiht classes and instances

--- car.py
+++ car.py
@@ -5,11 +5,51 @@ class Car:
         self.make = make
         self.model = model
         self.year = year
+        self.odometer_reading = 0
 
     def get_descriptive_name(self):
         """Return a neatly formatted descriptive name"""
         long_name = f"{self.year} {self.make} {self.model}"
         return long_name.title()
     
+    def read_odometer(self):
+        """Print a statement showing the car's milage."""
+        print(f"This car has {self.odometer_reading} miles on it.")
+
+    def update_odometer(self, milage):
+        """
+        Set the odometer reading to the given value.
+        Reject the change if it attempts to roll the odometer back.
+        """
+        if milage >= self.odometer_reading:
+            self.odometer_reading = milage
+        else:
+            print("You can not roll back an odometer!")
+
+    def increment_odometer(self, miles):
+        """
+        Add the given amount to the odometer reading
+        """
+        if miles > 0:
+            self.odometer_reading += miles
+        else:
+            print("You must enter a positive number to increment the odometer.")
+    
 my_new_car = Car('audi', 'a4', 2024)
 print(my_new_car.get_descriptive_name())
+my_new_car.update_odometer(55)
+my_new_car.read_odometer()
+my_new_car.update_odometer(22)
+my_new_car.read_odometer()
+
+my_used_car = Car('subaru', 'outback', 2019)
+print(my_used_car.get_descriptive_name())
+
+my_used_car.update_odometer(23_500)
+my_used_car.read_odometer()
+
+my_used_car.increment_odometer(100)
+my_used_car.read_odometer()
+
+my_used_car.increment_odometer(-50)
+my_used_car.read_odometer()
--- restaurant.py
+++ restaurant.py
@@ -3,6 +3,7 @@ class Restaurant:
     def __init__(self, name, type) -> None:
         self.name = name
         self.type = type
+        self.number_served = 0
 
     def describe(self):
         print(f"{self.name} serves {self.type} style food.")
@@ -10,10 +11,32 @@ class Restaurant:
     def open(self):
         print(f"The resurant {self.name} is open.")
 
+    def read_number_served(self):
+        print(f"There are {self.number_served} customers in the resturant.")
+
+    def set_number_served(self, customers):
+        if self.number_served >= customers:
+            self.number_served = customers
+        else:
+            print("You can not reduce your total number of customers.")
+
+    def increment_number_served(self, new_customers):
+        if new_customers > 0:
+            self.number_served += new_customers
+        else:
+            print("You need to enter a positive number.")
+
 resturant = Restaurant('In and Out', 'American')
 
 resturant.describe()
 resturant.open()
+resturant.read_number_served()
+resturant.set_number_served(50)
+resturant.read_number_served()
+resturant.set_number_served(40)
+resturant.increment_number_served(15)
+resturant.read_number_served()
+resturant.increment_number_served(-5)
 
 resturant = Restaurant('The Crab House', "Sea Food")
 resturant.describe()
